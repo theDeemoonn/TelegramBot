849fa727afa712202f348281f591a6e36642f0af
dima <deemoonn@gmail.com>
2022-04-28 14:37:59 +0300

    first commit

---
+++ help.js
@@ -0,0 +1,9 @@
+const helpCommands = `
+Если возникли вопросы или проблемы с ботом, пожалуйста, обратитесь к администратору бота @pravodoc_ru.
+
+`
+
+module.exports.helpcommands = helpCommands;
+
+
+//Сообщение помощи при вводе команды
